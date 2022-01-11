5c6f14ca1dbc5ffd8077580de7721b698ecc8243
teejayo7 <joshuataiwo07@gmail.com>
2022-01-11 10:58:41 +0100

    I made my Get Ready disappear after 1.5s

--- Assets/js/guess.js
+++ Assets/js/guess.js
@@ -3,16 +3,13 @@ function myFunction(){
     var guess = document.getElementById('number').value;
 
         if(secret < guess){
-            // alert(guess + ' is too high, try again');
             document.getElementById('number').style.borderColor =  "red"; 
-            document.getElementById('high').style.display =  "block"; 
+            document.getElementById('high').style.display =  "block";
         }
         else if(secret > guess){
-            // alert(guess + ' is too low, try again');
             document.getElementById('number').style.borderColor =  "red"; 
             document.getElementById('low').style.display =  "block";
         } else{
-            // alert('You guessed right');
         document.getElementById('number').style.borderColor =  "green";
         document.getElementById('right').style.display =  "block"; 
         }
@@ -26,6 +23,7 @@ function displayFunction(){
     document.getElementById('nodisplay').style.display = "none";
     document.getElementById('displayblock').style.display = "block";
     document.getElementById('ready').style.display = "block";
+    setTimeout(() => ready.remove(), 1500);
     
 }
 function noFunction(){
