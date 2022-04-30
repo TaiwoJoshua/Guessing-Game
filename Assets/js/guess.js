263983e475ebf2875351607ac04c8cd1a7055e93
teejayo7 <joshuataiwo07@gmail.com>
2022-04-30 03:42:45 +0100

    Added a response for no input

--- Assets/js/guess.js
+++ Assets/js/guess.js
@@ -12,17 +12,31 @@ function displayFunction(){
 function myFunction(){
     var secret = secrets * 2;
     var guess = document.getElementById('number').value;
-
-        if(secret < guess){
+        if(guess == ""){
+            document.getElementById('number').style.borderColor =  "red"; 
+            document.getElementById('none').style.display =  "block";
+            document.getElementById('high').style.display =  "none";
+            document.getElementById('low').style.display =  "none";
+            document.getElementById('right').style.display =  "none";
+        }else if(secret < guess){
             document.getElementById('number').style.borderColor =  "red"; 
             document.getElementById('high').style.display =  "block";
+            document.getElementById('none').style.display =  "none";
+            document.getElementById('low').style.display =  "none";
+            document.getElementById('right').style.display =  "none";
         }
         else if(secret > guess){
             document.getElementById('number').style.borderColor =  "red"; 
             document.getElementById('low').style.display =  "block";
-        } else{
-        document.getElementById('number').style.borderColor =  "green";
-        document.getElementById('right').style.display =  "block"; 
+            document.getElementById('high').style.display =  "none";
+            document.getElementById('none').style.display =  "none";
+            document.getElementById('right').style.display =  "none";
+        }else{
+            document.getElementById('number').style.borderColor =  "green";
+            document.getElementById('right').style.display =  "block"; 
+            document.getElementById('high').style.display =  "none";
+            document.getElementById('low').style.display =  "none";
+            document.getElementById('none').style.display =  "none";
         }
     };
 
